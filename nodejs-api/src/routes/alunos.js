e92cf5ef04231ed16bc80d51ead96654a0e579b2
iokanaan <iokannaan@hotmail.com>
2026-06-16 21:10:08 -0300

    tentando acertar o git

--- nodejs-api/src/routes/alunos.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express')
-const c = require('../controllers/alunoController')
-const router = Router()
-router.get('/', c.listar)
-router.get('/:ra', c.buscarPorRa)
-router.post('/', c.criar)
-router.put('/:ra', c.atualizar)
-router.delete('/:ra', c.remover)
-module.exports = router
--- nodejs-api/src/routes/empresas.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express')
-const c = require('../controllers/empresaController')
-const router = Router()
-router.get('/', c.listar)
-router.get('/:id', c.buscarPorId)
-router.post('/', c.criar)
-router.put('/:id', c.atualizar)
-router.delete('/:id', c.remover)
-module.exports = router
