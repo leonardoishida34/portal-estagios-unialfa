55210096175821402ca6f19595924f8aaecadfde
iokanaan <iokannaan@hotmail.com>
2026-06-17 18:15:20 -0300

    toques finais

--- nodejs-api/src/routes/empresas.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { listar, buscarPorId, criar, atualizar, remover } from '../controllers/empresaController'
-const router = Router()
-router.get('/', listar)
-router.get('/:id', buscarPorId)
-router.post('/', criar)
-router.put('/:id', atualizar)
-router.delete('/:id', remover)
-export default router
--- nodejs-api/src/routes/usuarios.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { listar, login, criar, remover } from '../controllers/usuarioController'
-const router = Router()
-router.get('/', listar)
-router.post('/login', login)
-router.post('/', criar)
-router.delete('/:id', remover)
-export default router
